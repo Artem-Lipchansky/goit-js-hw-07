e3274d9b198f2ed112b266ff7e188fdb34dcaa6b
Artem Lipchansky <lipchanskiy.an@gmail.com>
2024-01-26 20:39:26 +0200

    added

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+
+const categoriesList = document.getElementById('categories');
+
+const categoryCount = categoriesList.children.length;
+
+console.log(`Number of categories: ${categoryCount}`);
+
+
+categoriesList.querySelectorAll('.item').forEach(category => {
+    const categoryName = category.querySelector("h2").textContent;
+    const itemCount = category.querySelectorAll("li").length;
+
+    console.log(`Category: ${categoryName}`);
+    console.log(`Elements: ${itemCount}`);
+});
