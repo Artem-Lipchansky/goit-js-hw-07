da59dc22e1d34665e750b28f7de9664c20c652a8
Artem Lipchansky <lipchanskiy.an@gmail.com>
2024-01-28 22:57:52 +0200

    added

---
+++ js/task-3.js
@@ -0,0 +1,21 @@
+
+const enterName = document.getElementById('name-input');
+console.log(enterName);
+
+const resultName = document.getElementById('name-output');
+console.log(resultName);
+
+
+enterName.addEventListener('input', resultGreetings);
+
+
+function resultGreetings() {
+    const trimmedName = enterName.value.trim();
+    const outputName = trimmedName === '' ? 'Anonymus' : trimmedName;
+
+    resultName.textContent = outputName;
+}
+
+
+
+
